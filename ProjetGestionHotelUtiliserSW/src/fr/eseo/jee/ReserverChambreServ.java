433414bb987b479efd50dae092d608a9b05b9e15
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-03-12 08:45:41 +0100

    Tiffaine : Index et ReserverChambreServ Début

---
+++ ProjetGestionHotelUtiliserSW/src/fr/eseo/jee/ReserverChambreServ.java
@@ -0,0 +1,40 @@
+package fr.eseo.jee;
+
+import java.io.IOException;
+import java.util.Date; 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReserverChambreServ
+ */
+@WebServlet("/ReserverChambreServ")
+public class ReserverChambreServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReserverChambreServ() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int nbVoyageurs = Integer.parseInt(request.getParameter("nbVoyageurs")); 
+		int budgetMin = Integer.parseInt(request.getParameter("budgetMin")); 
+		int budgetMax = Integer.parseInt(request.getParameter("budgetMax")); 
+		String typeChambre = request.getParameter("typeChambre"); 
+		String dateArrivee = request.getParameter("dateArrivee"); 
+		String dateDepart = request.getParameter("dateDepart"); 
+
+	}
+
+}
